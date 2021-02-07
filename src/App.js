068743adaa6e771f6c71ffb9b8e6f34bac8854d3
LEEKYOOBAEK <charisma1124@gmail.com>
2021-02-08 00:09:23 +0900

    first commit before learning state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from 'react-dom';
+import PropTypes from "prop-types"
 
-function App() {
+
+const foodILike = [{ 
+  id: 1,
+  name: "Kimchi",
+  image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  rating: 5
+}, {
+  id: 2,
+  name: "Samgyeopsal",
+  image: "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  rating:4.3
+}, {
+  id: 3,
+  name: "Bibimbap",
+  image: "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+  rating:3.5
+}, {
+  id: 4,
+  name: "Kimbap",
+  image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+  rating: 4.8
+}];
+function Food({name,picture, rating}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>I love {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
   );
 }
 
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number
+};
+
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
+    </div>
+  );
+}
 export default App;
