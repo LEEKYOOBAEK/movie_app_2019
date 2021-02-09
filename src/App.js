a8e4ff7cc5abf31e3b605b7a80b8fbcfd037f1ab
LEEKYOOBAEK <charisma1124@gmail.com>
2021-02-09 11:27:10 +0900

    current

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends React.Component {
   };
   add = () => {
     console.log("add");
-    this.setState({count:this.state.count+1});
+    this.setState(current => ({count:current.count+1}));
   };
   minus = () => {
     console.log("minus");
-    this.setState({count:this.state.count-1});
+    this.setState(current => ({count:this.state.count-1}));
   };
 
   render (){
